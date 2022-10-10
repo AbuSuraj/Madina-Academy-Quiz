8724728fb2b8ebf43ca67d2e3bbaab7071f15ece
abu bokor suraj <abusurajbd@gmail.com>
2022-10-10 23:14:22 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Main from './Layout/Main';
+import Home from './compnents/Home/Home';
+import Statistics from './compnents/Statistics/Statistics';
+import Blog from './compnents/Blog/Blog';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path:'/',
+      element: <Main></Main>,
+      children:[
+        {
+          path:"/home",
+          
+          element:<Home></Home>
+        },
+        {
+          path:"/statistics",
+          element:<Statistics></Statistics>
+        },
+        {
+          path:"/blog",
+          element: <Blog></Blog>
+        },
+      ]
+    }
+  ])
   return (
-    <div className="App">
-      
+    <div className="">
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
--- src/compnents/Header/Header.js
+++ src/compnents/Header/Header.js
@@ -1,9 +1,24 @@
 import React from 'react';
-
+import { NavLink } from 'react-router-dom';
+let activeStyle = {
+    textDecoration: "",
+    backgroundColor: "#5F9DF7"
+  };
 const Header = () => {
     return (
-        <div>
-            <h1>header</h1>
+        <div className='navbar bg-base-100 flex-col md:flex-row justify-between  '>
+            <h1 className='text-3xl font-serif font-extrabold ml-5 ' >Madina Academy Quiz</h1>
+            <nav className='mr-12'>
+                <NavLink to={'/home'} style={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            } className="btn btn-secondary mr-4" >Home</NavLink>
+                <NavLink to={'/statistics'} style={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            } className="btn btn-secondary mr-4" >Statistics</NavLink>
+                <NavLink to={'/blog'} style={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            } className="btn btn-secondary mr-2" >Blog</NavLink>
+            </nav>
         </div>
     );
 };
